f126c6da964eb6dfa7ca9f8b0bda9a40e4744310
Akahal Vitalis <akahal.info@gmail.com>
2023-02-05 01:28:24 +0100

    Android Quiz App

---
+++ app/src/main/java/com/example/myschoolproject/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.myschoolproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button startBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startBtn = findViewById(R.id.startBtn);
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/myschoolproject/QuestionsBank.java
@@ -0,0 +1,130 @@
+package com.example.myschoolproject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuestionsBank {
+
+    private static List<QuestionsList> physQuestions() {
+
+        final List<QuestionsList> questionsLists = new ArrayList<>();
+
+        // Create object of questionsList class and pass a questions along with options and answer 38:14
+        final QuestionsList question1 = new QuestionsList("The inner diameter of a test tube can be measured accurately using a", "micrometer screw gauge", "pair of dividers", "metre rule", "pair of vernier calipers", "pair of vernier calipers", "");
+        final QuestionsList question2 = new QuestionsList("Two bodies have masses in the ratio of 3:1. They experience forces that impart to them acceleration in the ratio 2:9 respectively. Find the ratio of forces the masses experienced.", "1 : 4", "2 : 1", "2 : 3", "2 : 5", "2 : 3", "");
+        final QuestionsList question3 = new QuestionsList("Total internal reflection occurs when light moves from", "a dense medium to a less dense medium", "air to water", "water to glass", "a less dense to a dense medium", "water to glass", "");
+        final QuestionsList question4 = new QuestionsList("A rope is being used to pull a mass of 10kg vertically upward. Determine the tension in the rope if, starting from rest, the mass acquires a velocity of 4ms-1 in 8s [g = 10ms-2]", "5N", "50N", "105N", "95N", "105N", "");
+        final QuestionsList question5 = new QuestionsList("If the force and the velocity on a system are each reduced simultaneously by half, the power of the system is", "doubled", "reduced to a quarter", "reduced by half", "constant", "doubled", "");
+        final QuestionsList question6 = new QuestionsList("A semiconductor diode is used in rectifying alternating current into direct current mainly because of it.", "is non-linear", "allows current to flow in either direction", "allows current to flow only in one direction", "offers a high input resistance", "allows current to flow in either direction", "");
+
+
+        // add all questions to List <QuestionsList>
+        questionsLists.add(question1);
+        questionsLists.add(question2);
+        questionsLists.add(question3);
+        questionsLists.add(question4);
+        questionsLists.add(question5);
+        questionsLists.add(question6);
+
+        return questionsLists;
+
+    }
+
+//    ============================ chem question =========================
+
+    private static List<QuestionsList> chemQuestions() {
+
+        final List<QuestionsList> questionsLists = new ArrayList<>();
+
+        // Create object of questionsList class and pass a questions along with options and answer 38:14
+        final QuestionsList question1 = new QuestionsList("The inner diameter of a test tube can be measured accurately using a", "micrometer screw gauge", "pair of dividers", "metre rule", "pair of vernier calipers", "pair of vernier calipers", "");
+        final QuestionsList question2 = new QuestionsList("Two bodies have masses in the ratio of 3:1. They experience forces that impart to them acceleration in the ratio 2:9 respectively. Find the ratio of forces the masses experienced.", "1 : 4", "2 : 1", "2 : 3", "2 : 5", "2 : 3", "");
+        final QuestionsList question3 = new QuestionsList("Total internal reflection occurs when light moves from", "a dense medium to a less dense medium", "air to water", "water to glass", "a less dense to a dense medium", "water to glass", "");
+        final QuestionsList question4 = new QuestionsList("A rope is being used to pull a mass of 10kg vertically upward. Determine the tension in the rope if, starting from rest, the mass acquires a velocity of 4ms-1 in 8s [g = 10ms-2]", "5N", "50N", "105N", "95N", "105N", "");
+        final QuestionsList question5 = new QuestionsList("If the force and the velocity on a system are each reduced simultaneously by half, the power of the system is", "doubled", "reduced to a quarter", "reduced by half", "constant", "doubled", "");
+        final QuestionsList question6 = new QuestionsList("A semiconductor diode is used in rectifying alternating current into direct current mainly because of it.", "is non-linear", "allows current to flow in either direction", "allows current to flow only in one direction", "offers a high input resistance", "allows current to flow in either direction", "");
+
+
+        // add all questions to List <QuestionsList>
+        questionsLists.add(question1);
+        questionsLists.add(question2);
+        questionsLists.add(question3);
+        questionsLists.add(question4);
+        questionsLists.add(question5);
+        questionsLists.add(question6);
+
+        return questionsLists;
+
+    }
+
+
+    //    ============================ Maths question =========================
+
+    private static List<QuestionsList> mathQuestions() {
+
+        final List<QuestionsList> questionsLists = new ArrayList<>();
+
+        // Create object of questionsList class and pass a questions along with options and answer 38:14
+        final QuestionsList question1 = new QuestionsList("The inner diameter of a test tube can be measured accurately using a", "micrometer screw gauge", "pair of dividers", "metre rule", "pair of vernier calipers", "pair of vernier calipers", "");
+        final QuestionsList question2 = new QuestionsList("Two bodies have masses in the ratio of 3:1. They experience forces that impart to them acceleration in the ratio 2:9 respectively. Find the ratio of forces the masses experienced.", "1 : 4", "2 : 1", "2 : 3", "2 : 5", "2 : 3", "");
+        final QuestionsList question3 = new QuestionsList("Total internal reflection occurs when light moves from", "a dense medium to a less dense medium", "air to water", "water to glass", "a less dense to a dense medium", "water to glass", "");
+        final QuestionsList question4 = new QuestionsList("A rope is being used to pull a mass of 10kg vertically upward. Determine the tension in the rope if, starting from rest, the mass acquires a velocity of 4ms-1 in 8s [g = 10ms-2]", "5N", "50N", "105N", "95N", "105N", "");
+        final QuestionsList question5 = new QuestionsList("If the force and the velocity on a system are each reduced simultaneously by half, the power of the system is", "doubled", "reduced to a quarter", "reduced by half", "constant", "doubled", "");
+        final QuestionsList question6 = new QuestionsList("A semiconductor diode is used in rectifying alternating current into direct current mainly because of it.", "is non-linear", "allows current to flow in either direction", "allows current to flow only in one direction", "offers a high input resistance", "allows current to flow in either direction", "");
+
+
+        // add all questions to List <QuestionsList>
+        questionsLists.add(question1);
+        questionsLists.add(question2);
+        questionsLists.add(question3);
+        questionsLists.add(question4);
+        questionsLists.add(question5);
+        questionsLists.add(question6);
+
+        return questionsLists;
+
+    }
+
+    //    ============================ eng question =========================
+
+    private static List<QuestionsList> engQuestions() {
+
+        final List<QuestionsList> questionsLists = new ArrayList<>();
+
+        // Create object of questionsList class and pass a questions along with options and answer 38:14
+        final QuestionsList question1 = new QuestionsList("The inner diameter of a test tube can be measured accurately using a", "micrometer screw gauge", "pair of dividers", "metre rule", "pair of vernier calipers", "pair of vernier calipers", "");
+        final QuestionsList question2 = new QuestionsList("Two bodies have masses in the ratio of 3:1. They experience forces that impart to them acceleration in the ratio 2:9 respectively. Find the ratio of forces the masses experienced.", "1 : 4", "2 : 1", "2 : 3", "2 : 5", "2 : 3", "");
+        final QuestionsList question3 = new QuestionsList("Total internal reflection occurs when light moves from", "a dense medium to a less dense medium", "air to water", "water to glass", "a less dense to a dense medium", "water to glass", "");
+        final QuestionsList question4 = new QuestionsList("A rope is being used to pull a mass of 10kg vertically upward. Determine the tension in the rope if, starting from rest, the mass acquires a velocity of 4ms-1 in 8s [g = 10ms-2]", "5N", "50N", "105N", "95N", "105N", "");
+        final QuestionsList question5 = new QuestionsList("If the force and the velocity on a system are each reduced simultaneously by half, the power of the system is", "doubled", "reduced to a quarter", "reduced by half", "constant", "doubled", "");
+        final QuestionsList question6 = new QuestionsList("A semiconductor diode is used in rectifying alternating current into direct current mainly because of it.", "is non-linear", "allows current to flow in either direction", "allows current to flow only in one direction", "offers a high input resistance", "allows current to flow in either direction", "");
+
+
+        // add all questions to List <QuestionsList>
+        questionsLists.add(question1);
+        questionsLists.add(question2);
+        questionsLists.add(question3);
+        questionsLists.add(question4);
+        questionsLists.add(question5);
+        questionsLists.add(question6);
+
+        return questionsLists;
+
+    }
+
+    public static List<QuestionsList> getQuestions(String selectedTopicName){
+        switch (selectedTopicName){
+            case "phys":
+                return physQuestions();
+
+            case "chem":
+                return chemQuestions();
+
+            case "maths":
+                return mathQuestions();
+
+            default:
+                return engQuestions();
+        }
+    }
+}
